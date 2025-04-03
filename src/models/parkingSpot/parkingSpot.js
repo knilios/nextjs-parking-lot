f6e622abe695a6ee7c1f4923f1928f8e53ebd2a3
Kristawan Jaroenrad <kristawanja@gmail.com>
2025-04-03 14:27:59 +0700

    Fill parkingspot class with necessary methods

--- src/models/parkingSpot/parkingSpot.js
+++ src/models/parkingSpot/parkingSpot.js
@@ -1,8 +1,23 @@
 class ParkingSpot {
-    constructor() {
-        self.width;
-        self.length;
-        self.hight;
+    isFit(vehicle) {
+        return (vehicle.width - this.width) * (-1) >= 0 && 
+                (vehicle.length - this.length) * (-1) >= 0 && 
+                (vehicle.hight - this.hight) * (-1) >= 0;
+    }
+
+    isFull() {
+        if(!this.car) {
+            return true;
+        }
+        return false;
+    }
+
+    parkCar(car) {
+        this.car = car
+    }
+
+    removeCar() {
+        this.car = undefined
     }
 }
 
